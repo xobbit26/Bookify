47a53f5f9e1f368c9d3cc627b631e02ea92fe557
Станислав Наравецкас <xobbit26@gmail.com>
2024-10-21 17:38:06 +0300

    created booking errors

---
+++ Bookify.Domain/Bookings/BookingErrors.cs
@@ -0,0 +1,31 @@
+using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Bookings;
+
+public static class BookingErrors
+{
+    public static Error NotFound => new(
+        "Booking.NotFound",
+        "The booking with the specified identified was not found."
+    );
+
+    public static Error Overlap => new(
+        "Booking.Overlap",
+        "The current booking is overlapping with an existing one."
+    );
+
+    public static Error NotReserved => new(
+        "Booking.NotReserved",
+        "The booking is not pending."
+    );
+
+    public static Error NotConfirmed => new(
+        "Booking.NotConfirmed",
+        "The booking is not confirmed."
+    );
+
+    public static Error AlreadyStarted => new(
+        "Booking.AlreadyStarted",
+        "The booking has already started."
+    );
+}
