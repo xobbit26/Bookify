25f3cffa3ede0fedc8d04cb1a58b75f15eba633c
Станислав Наравецкас <xobbit26@gmail.com>
2024-10-21 17:01:13 +0300

    Added Error record to use it for throwing some error information in future;

---
+++ Bookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,7 @@
+namespace Bookify.Domain.Abstractions;
+
+public record Error(string Code, string Name)
+{
+    public static Error None = new Error(string.Empty, String.Empty);
+    public static Error NullValue = new Error("Error.NullValue", "Null value was provided");
+};
