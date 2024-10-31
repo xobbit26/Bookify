f968429d87febc19b047a6a6eabc87a9464e7067
Станислав Наравецкас <xobbit26@gmail.com>
2024-10-31 21:12:44 +0200

    added IBookingRepository.cs

---
+++ Bookify.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,9 @@
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken);
+    void Add(Booking booking);
+}
