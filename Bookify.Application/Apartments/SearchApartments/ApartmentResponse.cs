59b6ebfc538557a8f41a9997e2a1258cd0eb62a8
Станислав Наравецкас <xobbit26@gmail.com>
2024-11-02 20:20:53 +0200

    Add apartment search functionality
    Implemented `SearchApartmentsQuery` and related response classes to handle apartment search requests. Added `SearchApartmentsQueryHandler` to process search logic, including filtering out apartments with conflicting bookings based on specified dates.

---
+++ Bookify.Application/Apartments/SearchApartments/ApartmentResponse.cs
@@ -0,0 +1,11 @@
+namespace Bookify.Application.Apartments.SearchApartments;
+
+public class ApartmentResponse
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public decimal Price { get; init; }
+    public string Currency { get; init; }
+    public AddressResponse Address { get; set ; }
+}
---
+++ Bookify.Application/Apartments/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,8 @@
+using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed record SearchApartmentsQuery(
+    DateOnly StartDate,
+    DateOnly EndDate
+) : IQuery<IReadOnlyList<ApartmentResponse>>;
