63b11697b37f67b73f008d46f5bd4e3a1e558663
Станислав Наравецкас <xobbit26@gmail.com>
2024-11-05 21:37:38 +0200

    Add Entity Framework Core integration
    Integrated Entity Framework Core by registering ApplicationDbContext with a connection string from configuration. This ensures better manageability and scaling for database operations. Also added the necessary imports for Entity Framework Core utilization.

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@ using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,6 +18,14 @@ public static class DependencyInjection
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         services.AddTransient<IEmailService, EmailService>();
 
+        var connectionString = configuration.GetConnectionString("Database")
+                               ?? throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+        
         return services;
     }
 }
