1a6b508f1fd3d71c222d12c53b2bd6d93eb8d75f
Станислав Наравецкас <xobbit26@gmail.com>
2024-11-05 21:33:33 +0200

    Add ApplicationDbContext class to handle database operations
    Introduced the ApplicationDbContext class in the Infrastructure project. This class inherits from DbContext and implements the IUnitOfWork interface, streamlining database operations for the application. Additionally, updated settings to exclude certain files from code inspection.

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+internal sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options), IUnitOfWork
+{
+    //This method is implemented in DbContext, that's why compiler doesn't require to implement it in the current class
+    // public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    // {
+    //     var result = await base.SaveChangesAsync(cancellationToken);
+    //     return result;
+    // }
+}
