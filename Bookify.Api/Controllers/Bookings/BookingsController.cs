20faf1679286be262edcb8f5bc4d1bb1b4f91a55
Станислав Наравецкас <xobbit26@gmail.com>
2024-11-20 22:37:25 +0200

    added post method to BookingsController

--- Bookify.Api/Controllers/Bookings/BookingsController.cs
+++ Bookify.Api/Controllers/Bookings/BookingsController.cs
@@ -1,4 +1,5 @@
 using Bookify.Application.Bookings.GetBooking;
+using Bookify.Application.Bookings.ReserveBooking;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,12 +10,35 @@ namespace Bookify.Api.Controllers.Bookings;
 public class BookingsController(ISender sender) : ControllerBase
 {
     [HttpGet("{id:guid}")]
-    public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetBooking(Guid id, CancellationToken cancellationToken)
     {
         var query = new GetBookingQuery(id);
 
         var result = await sender.Send(query, cancellationToken);
 
         return result.IsSuccess ? Ok(result.Value()) : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ReserveBooking(
+        ReserveBookingRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var reserveBookingCommand = new ReserveBookingCommand(
+            request.ApartmentId,
+            request.UserId,
+            request.StartDate,
+            request.EndDate
+        );
+        
+        var result = await sender.Send(reserveBookingCommand, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+        
+        return CreatedAtAction(nameof(GetBooking), new {id = result.Value()}, result.Value());
     }
 }
---
+++ Bookify.Api/Controllers/Bookings/ReserveBookingRequest.cs
@@ -0,0 +1,8 @@
+namespace Bookify.Api.Controllers.Bookings;
+
+public sealed record ReserveBookingRequest(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate
+);
