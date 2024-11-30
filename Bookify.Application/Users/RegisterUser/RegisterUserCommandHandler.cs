b42bfa65f8691da3d0a944dd75a448017f9aa497
Станислав Наравецкас <xobbit26@gmail.com>
2024-11-30 22:49:39 +0200

    Add user registration feature
    Implement the user registration functionality with a command, handler, and validator pattern. Introduced `RegisterUserCommand` for capturing registration data and `RegisterUserCommandHandler` to process the registration logic using `IAuthenticationService`. Validations are handled by `RegisterUserCommandValidator`, ensuring required fields and password policies are adhered to.

---
+++ Bookify.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,36 @@
+using Bookify.Application.Abstractions.Authentication;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users;
+
+namespace Bookify.Application.Users.RegisterUser;
+
+public class RegisterUserCommandHandler(
+    IAuthenticationService authenticationService,
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork
+) : ICommandHandler<RegisterUserCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = User.Create(
+            new FirstName(request.FirstName),
+            new LastName(request.LastName),
+            new Email(request.Email)
+        );
+
+        var identityId = await authenticationService.RegisterAsync(
+            user,
+            request.Password,
+            cancellationToken
+        );
+
+        user.SetIdentityId(identityId);
+
+        userRepository.Add(user);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return user.Id;
+    }
+}
